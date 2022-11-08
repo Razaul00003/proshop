fbb80efc55035ddcd6cf7dd1d86d82ea6e91b6fa
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-08 11:21:25 +0600

    added api working and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,17 @@
 import express from "express";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHanlder } from "./middleware/errorMiddlewar.js";
 dotenv.config();
 connectDB();
+const app = express();
 
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHanlder);
 //ENVIRONMENTAL CONSTANT
 const PORT = process.env.PORT || 5000;
-
-const app = express();
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.map((product) => {
-    product._id === req.params.id;
-    res.json(product);
-  });
-});
 
 app.listen(PORT, console.log(PORT.cyan.underline));
