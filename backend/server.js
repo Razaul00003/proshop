73286ec6f77438f5e164f913e9071376c293ae94
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-09 18:44:58 +0600

    backend auth login setup

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import connectDB from "./config/db.js";
 import colors from "colors";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHanlder } from "./middleware/errorMiddlewar.js";
 dotenv.config();
 connectDB();
 const app = express();
+app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHanlder);
 //ENVIRONMENTAL CONSTANT
