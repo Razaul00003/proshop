a36134783be080ae07e14888fb5df8641f05f6dc
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-10 06:53:50 +0600

    login screen

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,39 @@
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from "../constants/userConstants";
+import axios from "axios";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    //request user login
+    dispatch({ type: USER_LOGIN_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+
+    //dispatch on user login success
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
