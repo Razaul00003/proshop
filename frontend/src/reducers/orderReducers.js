c60c61213534aa30583e49b3abb4530ef62a1ab2
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-15 11:53:44 +0600

    fixed issue on ordersummary page

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -14,8 +14,12 @@ import {
   ORDER_LIST_MY_FAIL,
   ORDER_LIST_MY_RESET,
   ORDER_LIST_FAIL,
-  ORDER_LIST_REQUEST,
   ORDER_LIST_SUCCESS,
+  ORDER_LIST_REQUEST,
+  ORDER_DELIVER_FAIL,
+  ORDER_DELIVER_SUCCESS,
+  ORDER_DELIVER_REQUEST,
+  ORDER_DELIVER_RESET,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -88,6 +92,29 @@ export const orderPayReducer = (state = {}, action) => {
   }
 };
 
+export const orderDeliverReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_DELIVER_REQUEST:
+      return {
+        loading: true,
+      };
+    case ORDER_DELIVER_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+    case ORDER_DELIVER_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case ORDER_DELIVER_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
 export const orderListMyReducer = (state = { orders: [] }, action) => {
   switch (action.type) {
     case ORDER_LIST_MY_REQUEST:
@@ -105,7 +132,7 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
         error: action.payload,
       };
     case ORDER_LIST_MY_RESET:
-      return { order: [] };
+      return { orders: [] };
     default:
       return state;
   }
